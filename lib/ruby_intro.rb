e58a89b548180910a21877ada265b5e4a52e460a
EC2 Default User <ec2-user@ip-172-31-24-191.ec2.internal>
2019-01-02 02:13:24 +0000

    Finished A1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,49 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each { |a| sum=sum+a }
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.size == 0 
+  return arr.at(0) if arr.size == 1
+  return arr.sort.last(2).sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).any? {|a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[bcdfghjklmnpqrstvwxyz]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  /^(0|(10|111*0)(10|111*0)*0)*$/.match(s) != nil if !s.empty?
 end
 
 # Part 3
 
-class BookInStock
-# YOUR CODE HERE
+class BookInStock 
+  
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def initialize(isbn, price)
+        @isbn = isbn
+        raise ArgumentError, "ISBN is empty string" unless !isbn.empty?
+        @price = price
+        raise ArgumentError, "Price should not be zero" unless price > 0
+  end
+  def price_as_string
+    return "$" + sprintf('%.2f', price)
+  end
 end
